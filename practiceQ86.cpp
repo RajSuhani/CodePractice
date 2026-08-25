4277aebe55666a3a77ea8c23e2abc3515c7b9d71
RajSuhani <ak8045561@gmail.com>
2026-08-25 16:16:53 +0530

    Day 71: Smallest Missing Multiple of K

---
+++ practiceQ86.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int missingMultiple(vector<int>& nums, int k) {
+        unordered_set<int> st(nums.begin(), nums.end());
+
+        int x = k;
+
+        while (st.count(x)) {
+            x += k;
+        }
+
+        return x;
+    }
+};
