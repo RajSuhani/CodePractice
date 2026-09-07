9eb1e3feade91118b5bfd60aeb4a5a381f02eafc
RajSuhani <ak8045561@gmail.com>
2026-09-07 17:58:01 +0530

    Day 80: Distinct Subsequences II

---
+++ practiceQ99.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int distinctSubseqII(string s) {
+        const long long MOD = 1e9 + 7;
+        
+        vector<long long> last(26, 0);
+        long long total = 0;
+        
+        for (char c : s) {
+            int x = c - 'a';
+            
+            long long newSubseq = (total + 1) % MOD;
+            total = (total + newSubseq - last[x] + MOD) % MOD;
+            last[x] = newSubseq;
+        }
+        
+        return total;
+    }
+};
